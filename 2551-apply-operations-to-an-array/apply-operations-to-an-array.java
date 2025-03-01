b8a0109c14d50261fd8c9b524ca504fbc368c29e
Ananya singh <sananya0104@gmail.com>
2025-03-01 19:46:42 +0530

    Time: 1 ms (96.37%) | Memory: 44.5 MB (5.12%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,31 +1,19 @@
 class Solution {
-
-    // Method to apply operations on an array of integers
     public int[] applyOperations(int[] nums) {
-        int length = nums.length; // Get the length of the array
-
-        // Loop through each element, except the last one
+        int length = nums.length; 
         for (int i = 0; i < length - 1; ++i) {
-            // Check if the current element is equal to the next element
             if (nums[i] == nums[i + 1]) {
-                // If so, double the current element
-                nums[i] <<= 1; // Same as nums[i] = nums[i] * 2
-                // And set the next element to zero
+                nums[i] <<= 1; 
                 nums[i + 1] = 0;
             }
         }
-
-        int[] result = new int[length]; // Create a new array to store the results
-        int index = 0; // Initialize result array index
-
-        // Iterate through the original array
+        int[] result = new int[length]; 
+        int index = 0;
         for (int num : nums) {
-            // Copy non-zero elements to the result array
             if (num > 0) {
-                result[index++] = num; // Assign and then increment the index
+                result[index++] = num;
             }
         }
-
-        return result; // Return the resulting array
+        return result; 
     }
 }
